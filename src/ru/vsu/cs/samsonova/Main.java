462ae2e48fcaf8925feed64c25e7ba431d3c3880
Anastasiya Samsonova <nastya.samsonova02@mail.ru>
2020-11-12 20:20:41 +0300

    task4-20

--- src/ru/vsu/cs/samsonova/Main.java
+++ src/ru/vsu/cs/samsonova/Main.java
@@ -10,18 +10,20 @@ public class Main {
         double result = putValuesOfElements(n);
         System.out.println(result);
     }
-    public static double putValuesOfElements(int n){
+
+    public static double putValuesOfElements(int n) {
         double result = 0;
         for (int i = 1; i <= n; i++) {
             result += calculateValuesOfElements(i);
         }
         return result;
     }
+
     private static double calculateValuesOfElements(int i) {
-        if (i == 1){
+        if (i == 1) {
             return 0;
         }
-        if (i == 2){
+        if (i == 2) {
             return 0.625;
         }
         return 0.5 * calculateValuesOfElements(i - 1) + 0.75 * calculateValuesOfElements(i - 2);

6077924364da5bb8e2fb1a626b9d260295b80fc0
Anastasiya Samsonova <nastya.samsonova02@mail.ru>
2020-12-14 16:10:40 +0300

    task4-20

--- src/ru/vsu/cs/samsonova/Main.java
+++ src/ru/vsu/cs/samsonova/Main.java
@@ -5,27 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        double result = putValuesOfElements(n);
+        int n = enterCountElements();
+        double result = putElementsValues(n);
         System.out.println(result);
     }
 
-    public static double putValuesOfElements(int n) {
+    private static int enterCountElements() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Count elements in sequence ");
+        return scanner.nextInt();
+    }
+
+    public static double putElementsValues(int n) {
         double result = 0;
         for (int i = 1; i <= n; i++) {
-            result += calculateValuesOfElements(i);
+            result += calculateElementsValues(i);
         }
         return result;
     }
 
-    private static double calculateValuesOfElements(int i) {
+    private static double calculateElementsValues(int i) {
         if (i == 1) {
             return 0;
         }
         if (i == 2) {
             return 0.625;
         }
-        return 0.5 * calculateValuesOfElements(i - 1) + 0.75 * calculateValuesOfElements(i - 2);
+        return 0.5 * calculateElementsValues(i - 1) + 0.75 * calculateElementsValues(i - 2);
     }
 }
